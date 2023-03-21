d78925ac7ffc9010f8636a8e78bd92beb43d6d00
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-21 16:22:03 +0000

    bb

--- main.tf
+++ main.tf
@@ -1,4 +1,22 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags = merge(var.tags,{Name="${var.env}-vpc"})
+  tags = merge(
+    var.tags,
+    {Name="${var.env}-vpc"}
+    
+  )
+}
+
+
+## Public Subnets
+resource "aws_subnet" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+
+  for_each          = var.public_subnets
+  cidr_block        = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
 }
