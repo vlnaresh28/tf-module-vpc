34a880ef3caccc4d5475754ddfddb2f24d869158
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-21 05:58:29 +0000

    ff

--- vars.tf
+++ vars.tf
@@ -1,7 +1,2 @@
 variable "env" {}
 variable "vpc_cidr" {}
-variable "tags" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-variable "default_vpc_id" {}
-variable "default_route_table" {}
