44ea2c3a92d60e34a5eaa2fab3481c80d0058043
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-22 10:48:14 +0000

    gr

--- main.tf
+++ main.tf
@@ -134,3 +134,12 @@ resource "aws_route_table_association" "private-association" {
   subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
   route_table_id = aws_route_table.private-route-table[each.value["name"]].id
 }
+
+
+## Route to the default VPC for peering to work.
+
+resource "aws_route" "route" {
+  route_table_id            = var.default_route_table
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
