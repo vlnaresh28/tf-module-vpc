6726834d7c0232258b3ea5be83270850b77043c6
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-22 04:59:44 +0000

    ng

--- main.tf
+++ main.tf
@@ -22,11 +22,25 @@ resource "aws_subnet" "public_subnets" {
 }
 
 
+## Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-igw" }
+  )
+}
+
+
 # Public Route table
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
 
-
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   for_each = var.public_subnets
   tags = merge(
