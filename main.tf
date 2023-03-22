5ae5828c063a634156acbc1b01f5342230d827c6
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-22 08:56:08 +0000

    tt

--- main.tf
+++ main.tf
@@ -92,6 +92,14 @@ resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.main.id
 
   for_each = var.private_subnets
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateways["public-${split("-", each.value["name"])[1]}"].id
+  }
+  route {
+    cidr_block                = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
   tags = merge(
     var.tags,
     { Name = "${var.env}-${each.value["name"]}" }
